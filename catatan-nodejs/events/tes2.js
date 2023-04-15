cb035a77e942f02ea32c83f52db8a3ac52de399a
belaekaputri <belaekaputri28@gmail.com>
2023-04-15 20:54:23 +0700

    edit baliak

--- catatan-nodejs/events/tes2.js
+++ catatan-nodejs/events/tes2.js
@@ -1,8 +1,8 @@
 const {EventEmitter}= require('events'); 
  const myEmitter= new EventEmitter();
- const birthdayEventListener=({name})=>{
+ const birthdayEventListener=(name)=>{
     console.log(`생일 축하해요 ${name}`);
  }
 
 myEmitter.on('birthday',birthdayEventListener); 
- myEmitter.emit('birthday',{name:'김한빈'}); 
+ myEmitter.emit('birthday','김한빈'); 
